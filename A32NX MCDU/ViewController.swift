dbe0e3fccbda6e78e36276b9de914536d2063561
Tyler Knox <tyler58546@gmail.com>
2021-02-07 15:00:58 -0800

    Initial Commit

---
+++ A32NX MCDU/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A32NX MCDU
+//
+//  Created by Tyler Knox on 2/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
